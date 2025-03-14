7e2dc00f668746ff9b6ca908f2b4aa13f1377756
TpowlesGaming <t.powlesgaming@gmail.com>
2025-03-14 15:27:24 +0000

    Added Movement to player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField]
+    float moveSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Declare Variables
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = Input.GetAxis("Jump") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        // Move by values
+        transform.Translate(xValue, yValue, zValue);
     }
 }
